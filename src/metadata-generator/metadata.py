b487871e91bce32749fa6dec5c619bfc90e18c7d
UlVc <ulvc@ciencias.unam.mx>
2021-10-17 12:47:15 -0500

    Connection with the api

--- src/metadata-generator/metadata.py
+++ src/metadata-generator/metadata.py
@@ -1,21 +1,26 @@
 import json
+import requests
+from requests.structures import CaseInsensitiveDict
 
 f = open('_metadata.json',)
 count = 1
 data = json.load(f)
+url = 'https://api.nft-maker.io/UploadNft/45695b5532024df8804631b51f5e5124/19651'
+headers = CaseInsensitiveDict()
+headers["Accept"] = "application/json"
+headers["Content-Type"] = "application/json"
 
 for nft in data:
-  description = nft['attributes'][0]['value'] + " background\n" + nft['attributes'][5]['value'] + " eyes\n" + nft['attributes'][2]['value'] + " skin\n" + nft['attributes'][1]['value'] + " beak\n" + nft['attributes'][3]['value'] + " clothe\n" + nft['attributes'][6]['value'] + " hat\n" + nft['attributes'][4]['value'] + " accesory"
   mensaje = {
 			    "assetName": "ToughPenguin" + str(count),
 			    "previewImageNft": {
 			    "mimetype": "image/png",
 			    "fileFromIPFS": "QmQu7DAi9k4aDXCbpzXdDxVuMtha5mWJgm58mZEPUsx6vQ/" + str(count) + ".png",
-			    "description": description
+			    "description": "10k Tough Penguins rockin' on the Cardano blockchain"
 			    }
 			}
-  y = json.dumps(mensaje)
-  print(y)
+  resp = requests.post(url, headers=headers, data=json.dumps(mensaje))
+  print(resp.text)
   count += 1
 
 f.close()
